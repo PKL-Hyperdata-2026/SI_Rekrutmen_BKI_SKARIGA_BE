904bfbcfa075a4cf7fd3466a9193e3eec97c60b5
Marvellbrazil <marvellbrazil@gmail.com>
2026-08-24 16:13:08 +0700

    feat(view): add reset-password view and its preview

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('preview')->group(function () {
+    Route::get('/reset-password', function () {
+        return view('emails.reset-password');
+    });
+});

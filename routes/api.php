120eb11b4d3439033d76030070e7f3f0238e3389
SuryaJayanata <sjayanata00@gmail.com>
2026-09-04 15:39:58 +0700

    refactor(portfolio): expand social media platforms and streamline student routes

--- routes/api.php
+++ routes/api.php
@@ -74,24 +74,14 @@ Route::middleware('auth:sanctum')->group(function () {
         // TODO: API untuk HRD
     });
 
-    Route::middleware('role:siswa')->prefix('siswa')->group(function () {
+    Route::middleware('role:siswa,alumni')->prefix('siswa')->group(function () {
         Route::get('/portfolio/profile', [PortfolioController::class, 'getProfile']);
         Route::get('/portfolio/options', [PortfolioController::class, 'getOptions']);
         Route::put('/portfolio/profile', [PortfolioController::class, 'updateProfile']);
         Route::post('/portfolio/upload', [PortfolioController::class, 'uploadPortfolio']);
         Route::delete('/portfolio/{portfolio}', [PortfolioController::class, 'destroyPortfolio']);
 
-        Route::middleware('role:siswa,alumni')->group(function () {
-            Route::get('/my-applications', [StudentJobApplicationController::class, 'index']);
-            Route::get('/my-applications/{id}', [StudentJobApplicationController::class, 'show']);
-        });
-
-        Route::middleware('role:alumni')->prefix('alumni')->group(function () {
-            Route::get('/portfolio/profile', [PortfolioController::class, 'getProfile']);
-            Route::get('/portfolio/options', [PortfolioController::class, 'getOptions']);
-            Route::put('/portfolio/profile', [PortfolioController::class, 'updateProfile']);
-            Route::post('/portfolio/upload', [PortfolioController::class, 'uploadPortfolio']);
-            Route::delete('/portfolio/{portfolio}', [PortfolioController::class, 'destroyPortfolio']);
-        });
+        Route::get('/my-applications', [StudentJobApplicationController::class, 'index']);
+        Route::get('/my-applications/{id}', [StudentJobApplicationController::class, 'show']);
     });
 });
